9884f555f3b27ac0eef2d82beb59f805180c04d4
Kevin Sitzes <ksitzes@amperecomputing.com>
2023-04-02 07:17:46 -0500

    chore: pagination update

--- app.js
+++ app.js
@@ -1,48 +1,90 @@
+// Set the current page and items per page
+let currentPage = 1;
+const itemsPerPage = 100;
+
 function indexPage() {
+  fetchDataAndRender(currentPage);
+
+  function fetchDataAndRender(page) {
     fetch('https://stampchain.io/stamp.json')
-    .then(response => response.json())
-    .then(data => {
-        const dataContainer = document.getElementById('data-container');
-        data.reverse().forEach((item, index) => {
-            const itemContainer = document.createElement('div');
-            itemContainer.classList.add('item');
-            if (item.stamp_url) {
-                const img = document.createElement('img');
-                img.src = item.stamp_url;
-                img.width = 210;
-                img.height = 210;
-                img.onerror = function() {
-                    this.onerror = null;
-                    this.src = 'images/sad.png';
-                };
-                img.style.objectFit = 'contain';
-                img.style.imageRendering = 'pixelated';
-                img.style.imageRendering = '-moz-crisp-edges';
-                img.style.imageRendering = 'crisp-edges';
-                img.style.backgroundColor = '#000000';
-                itemContainer.appendChild(img);
-            }
-            const stampInfo = document.createElement('pre');
-            stampInfo.innerText = `Stamp: ${item.stamp}`;
-            itemContainer.appendChild(stampInfo);
+      .then(response => response.json())
+      .then(data => {
+        const startIndex = (page - 1) * itemsPerPage;
+        const endIndex = page * itemsPerPage;
+        const pageData = data.slice(startIndex, endIndex);
+        renderData(pageData);
+        renderPaginationButtons(page, data.length);
+      })
+      .catch(error => console.error(error));
+  }
 
-            const txHashInfo = document.createElement('pre');
-            const truncatedTxHash = `${item.tx_hash.slice(0, 5)}...${item.tx_hash.slice(-5)}`;
-            txHashInfo.innerText = `Tx Hash: ${truncatedTxHash}`;
-            itemContainer.appendChild(txHashInfo);
+  function renderData(data) {
+    const dataContainer = document.getElementById('data-container');
+    // Clear the previous items from the container
+    dataContainer.innerHTML = '';
 
-            const viewMoreBtn = document.createElement('button');
-            viewMoreBtn.innerText = 'View More';
-            viewMoreBtn.addEventListener('click', () => window.location.href = `asset.html?stampNumber=${item.stamp}`);
-            //viewMoreBtn.addEventListener('click', () => window.open(`https://xchain.io/asset/${item.asset}`, '_blank'));
-            itemContainer.appendChild(viewMoreBtn);
-            dataContainer.appendChild(itemContainer);
-            //if ((index + 1) % 4 === 0) {
-            //dataContainer.appendChild(document.createElement('br'));
-            //}
-        });
-    })
-    .catch(error => console.error(error));
+    data.reverse().forEach((item, index) => {
+      const itemContainer = document.createElement('div');
+      itemContainer.classList.add('item');
+      if (item.stamp_url) {
+        const img = document.createElement('img');
+        img.src = item.stamp_url;
+        img.width = 210;
+        img.height = 210;
+        img.onerror = function () {
+          this.onerror = null;
+          this.src = 'images/sad.png';
+        };
+        img.style.objectFit = 'contain';
+        img.style.imageRendering = 'pixelated';
+        img.style.imageRendering = '-moz-crisp-edges';
+        img.style.imageRendering = 'crisp-edges';
+        img.style.backgroundColor = '#000000';
+        itemContainer.appendChild(img);
+      }
+      const stampInfo = document.createElement('pre');
+      stampInfo.innerText = `Stamp: ${item.stamp}`;
+      itemContainer.appendChild(stampInfo);
+
+      const txHashInfo = document.createElement('pre');
+      const truncatedTxHash = `${item.tx_hash.slice(0, 5)}...${item.tx_hash.slice(-5)}`;
+      txHashInfo.innerText = `Tx Hash: ${truncatedTxHash}`;
+      itemContainer.appendChild(txHashInfo);
+
+      const viewMoreBtn = document.createElement('button');
+      viewMoreBtn.innerText = 'View More';
+      viewMoreBtn.addEventListener('click', () => window.location.href = `asset.html?stampNumber=${item.stamp}`);
+      itemContainer.appendChild(viewMoreBtn);
+      dataContainer.appendChild(itemContainer);
+    });
+  }
+
+  function renderPaginationButtons(page, totalItems) {
+    const paginationContainer = document.getElementById('pagination-container');
+    // Clear the previous buttons from the container
+    paginationContainer.innerHTML = '';
+
+    const totalPages = Math.ceil(totalItems / itemsPerPage);
+
+    const prevButton = document.createElement('button');
+    prevButton.innerText = '< Previous';
+    prevButton.disabled = page === 1;
+    prevButton.addEventListener('click', () => {
+      currentPage--;
+      fetchDataAndRender(currentPage);
+    });
+
+    const nextButton = document.createElement('button');
+    nextButton.innerText = 'Next >';
+    nextButton.disabled = page === totalPages;
+    nextButton.addEventListener('click', () => {
+      currentPage++;
+      fetchDataAndRender(currentPage);
+    });
+
+    paginationContainer.appendChild(prevButton);
+    paginationContainer.appendChild(nextButton);
+  }
 }
 
 
